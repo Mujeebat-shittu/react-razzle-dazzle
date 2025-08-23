846f469b785eec18b240050f43e97219297e05bb
Mujeebat Shittu <shittuhoney@gmail.com>
2025-08-23 17:58:43 +0100

    changed navigation styling

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,50 +1,98 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
-import { MenuIcon } from "lucide-react";
+// import { useState } from "react";
+// import { MenuIcon } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardAction,
+} from "@/components/ui/card";
+import { ExternalLink } from "lucide-react";
+
 
 const NavBar = function () {
-  const [dropDown, setDropDown] = useState(false);
-  const toggleMenu = function () {
-    setDropDown((prev) => !prev);
-  };
+  // const [dropDown, setDropDown] = useState(false);
+  // const toggleMenu = function () {
+  //   setDropDown((prev) => !prev);
+  // };
+
+
+  const cardData = [
+    {
+      key: "accordion",
+      label: "Accordion",
+      content: "A collapsible content display that keeps information tidy and accessible.",
+      link: <Link className="italic text-sm" to="/accordion">View Here</Link>
+    },
+    {
+      key: "cp",
+      label: "Colour Picker",
+      content: "An interactive tool that allows users to select and apply selected colors instantly",
+      link:  <Link className="italic text-sm" to="/colourPicker">View Here</Link>
+
+    },
+    {
+      key: "counter",
+      label: "Counter",
+      content: "An interactive counter with increment, decrement and reset controls to showcase state management.",
+      link: <Link className="italic text-sm" to="/counter">View Here</Link>
+
+    },
+    {
+      key: "faq",
+      label: "FAQs Toggle",
+      content: "A dynamic FAQ section where users can expand questions to reveal answers.",
+      link: <Link className="italic text-sm" to="/faq">View Here</Link>
+    },
+    {
+      key: "mt",
+      label: "Light/Dark Mode Toggle",
+      content: "A smooth theme switcher that lets users choose between light and dark experiences",
+      link: <Link className="italic text-sm" to="/modeToggle">View Here</Link>
+
+    },
+    {
+      key: "pt",
+      label: "Show/Hide Password Toggle",
+      content: "A user-friendly feature that makes entering passwords secure yet convenient.",
+      link: <Link className="italic text-sm" to="/passwordToggle">View Here</Link>
+
+    },
+    {
+      key: "quote",
+      label: "Random Quote Picker",
+      content: "A fun generator that displays a pre-programmed quotes with each click",
+      link: <Link className="italic text-sm" to="/quote">View Here</Link>
+
+    },
+    {
+      key: "tab",
+      label: "Tab Switcher",
+      content: "A clean, organized way to navigate between multiple content sections seamlessly.",
+      link: <Link className="italic text-sm" to="/tabs">View Here</Link>
+
+    }
+  ]
+
   return (
     <>
-      <nav className="no-underline text-[hsl(0,10%,50%)]">
-        <div className="relative group flex cursor-pointer ">
-          <MenuIcon
-            size={40}
-            className="static mx-auto my-44 top-4 right-8 flex items-center justify-center text-white text-[2rem]"
-            onClick={toggleMenu}
-          />
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 p-4 my-6 justify-items-center justify-center max-w-5xl mx-auto">
+        {
+          cardData.map((card) => (
+            <Card key={card}
+            className="w-[70%] lg:w-[90%] py-6 px-8 flex gap-6 text-left hover:scale-[1.05] hover:border-[hsl(10,0%,20%)] rotate-[1deg] hover:rotate-[0deg] translate-x-[-5%] cursor-pointer ">
+              <CardContent className="text-left space-y-2">
+                <CardHeader className="text-xl">{card.label}</CardHeader>
+                <CardDescription className="text-lg">{card.content}</CardDescription>
+                <CardAction>{card.link}</CardAction>
+              </CardContent>
 
-          <span
-            className="absolute top-20 left-1/2 -translate-x-1/2 mb-3 
-                       px-3 py-1 text-lg text-white bg-[hsl(0,0%,40%)] rounded 
-                       opacity-0 group-hover:opacity-100 
-                       transition-opacity duration-200 whitespace-nowrap"
-          >
-            Click on the icon to open or close the dropdown menu
-          </span>
-        </div>
+            </Card>
+          ))
+        }
 
-        <div
-          className="p-4 flex-col gap-4 top-20 right-0 bg-[hsl(0,0%,83%)] md:bg-none md:right-0 
-          md:flex-col md:gap-4 md:overflow-hidden w-[300px] text-center md:absolute md:top-16 md:left-1/2 md:-translate-x-1/2 
-           md:h-auto md:items-center md:justify-center"
-          style={{
-            display: dropDown ? "flex" : "none",
-          }}
-        >
-          <Link to="/accordion">Accordion</Link>
-          <Link to="/colourPicker">Colour Picker</Link>
-          <Link to="/counter">Counter</Link>
-          <Link to="/faq">FAQ</Link>
-          <Link to="/modeToggle">Mode Toggle</Link>
-          <Link to="/passwordToggle">Password Toggle</Link>
-          <Link to="/quote">Quote</Link>
-          <Link to="/tabs">Tab Switcher</Link>
-        </div>
-      </nav>
+      </div>
     </>
   );
 };
